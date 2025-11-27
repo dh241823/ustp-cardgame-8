763ae20daaca8b383a35145f9c2ba94e7f1fe4a5
Lukas Iselor <dh241823@fhstp.ac.at>
2025-11-27 09:03:42 +0100

    Update GitHub Actions workflow to specify upload path and improve deployment clarity

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ustp-cardgame-8/",
+
   plugins: [
     react(),
     tailwindcss(),
